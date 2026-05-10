bd771db2bef9a4df86cd6f360eae7a38233bcd0c
daisydu1012 <daisydu1012@gmail.com>
2026-05-10 14:46:24 -0700

    Optimize normalized batch queries with indexes

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,6 +1,6 @@
-CREATE INDEX ON tweet_tags (tag, id_tweets);
-CREATE INDEX ON tweet_tags (id_tweets, tag);
-CREATE INDEX ON tweets (id_tweets, lang);
-CREATE INDEX ON tweets
-USING GIN (to_tsvector('english', text))
+CREATE INDEX IF NOT EXISTS tweets_text_coronavirus_en_idx
+ON tweets USING gin (to_tsvector('english', text))
 WHERE lang = 'en';
+
+CREATE INDEX IF NOT EXISTS tweet_tags_id_tweets_tag_idx
+ON tweet_tags (id_tweets, tag);
